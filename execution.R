c84a8782b0838268ded210b6a2cced4dfc44df1b
Jodie Wiggins <jodiewiggins18@gmail.com>
2019-01-10 06:50:35 -0500

    test balance with sim trees

--- execution.R
+++ execution.R
@@ -1,10 +1,48 @@
+setwd("~/packages/balanced_tree")
+
+source("libraries.R")
+source("functions.R")
+
+#this is plotting different trees..
+layout(matrix(1:6, 3, 2))
+for (i in 1:3) plot(rcoal(20))
+for (i in 1:3) plot(balance(rcoal(20)))
+
+layout(matrix(1:6, 3, 2))
+for (i in 1:3) plot(rcoal(50))
+for (i in 1:3) plot(balance(rcoal(50)))
+
+#to plot the same tree ladderized and "balanced"
+nreps=3
+par(mfrow=c(nreps,2))
+for (i in sequence(nreps)) {
+  phy <- ape::rcoal(50)
+  plot(phy)
+  plot(balance(phy))
+}
+
+nreps=3
+par(mfrow=c(nreps,2))
+for (i in sequence(nreps)) {
+  phy <- ape::rcoal(20)
+  plot(phy)
+  plot(balance(phy))
+}
+
+#trees are too compact
+nreps=3
+par(mfrow=c(nreps,2))
+for (i in sequence(nreps)) {
+  phy <- ape::rcoal(100)
+  plot(phy)
+  plot(balance(phy))
+}
 
 
-source(libraries.R)
-source(functions.R)
-
-#Simulate Tree
-tree_20 <- rtree(n = 20)
-
-#Use balance function
-t20_b <- balance(tree_20)
+nreps=2
+par(mfrow=c(nreps,2))
+for (i in sequence(nreps)) {
+  phy <- ape::rcoal(100)
+  plot(phy)
+  plot(balance(phy))
+}
